001554e8a93c141c088fce0c43488d7d974abddb
Kauri <kauri.off@gmail.com>
2024-08-05 22:13:27 +0300

    I think i smart

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,22 @@
 use std::io;
 
-use curl::easy::{Easy, List};
 use serde_json::{to_string, Value};
 
 pub struct Chat {
     pub chat_id: String,
-    pub name: String
+    pub name: String,
 }
 
-pub fn get_messages(chat_id: &str, token: &str, full: bool) -> String {
+pub async fn get_messages(chat_id: &str, token: &str, full: bool) -> String {
     if full {
-        return get_messages_full(chat_id, token);
+        return get_messages_full(chat_id, token).await;
     } else {
-        return get_messages_count(chat_id, token, 50);
+        return get_messages_count(chat_id, token, 50).await;
     }
 }
 
-pub fn get_chats(token: &str) -> Vec<Chat> {
-    let res = discord_api("https://discord.com/api/v9/users/@me/channels", token);
+pub async fn get_chats(token: &str) -> Vec<Chat> {
+    let res = discord_api("https://discord.com/api/v9/users/@me/channels", token).await;
     let parsed: Vec<Value> = serde_json::from_str(&res).unwrap();
     let mut result: Vec<Chat> = Vec::new();
 
@@ -29,38 +28,31 @@ pub fn get_chats(token: &str) -> Vec<Chat> {
         for users in array_user {
             user_name = users["username"].to_string();
         }
-        result.push(Chat { chat_id: id, name: user_name });
+        result.push(Chat {
+            chat_id: id,
+            name: user_name,
+        });
     }
 
     result
 }
 
-fn discord_api(url: &str, token: &str) -> String {
-    let mut easy = Easy::new();
-
-    let header = format!("Authorization: {}", token);
-    let mut list = List::new();
-    list.append(&header).unwrap();
-
-    easy.url(&url).unwrap();
-    easy.http_headers(list).unwrap();
-
-    let mut data = Vec::new();
-    {
-        let mut transfer = easy.transfer();
-        transfer.write_function(|new_data| {
-            data.extend_from_slice(new_data);
-            Ok(new_data.len())
-        }).unwrap();
-
-        transfer.perform().unwrap();
-    }
-    std::str::from_utf8(&data).expect("Error parse UTF-8").to_string()
+async fn discord_api(url: &str, token: &str) -> String {
+    let client = reqwest::Client::new();
+    client
+        .get(url)
+        .header("Authorization", token)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap()
 }
 
-fn get_messages_full(chat_id: &str, token: &str) -> String {
+async fn get_messages_full(chat_id: &str, token: &str) -> String {
     let mut result = Vec::new();
-    let mut last_message = get_messages_count(chat_id, token, 50);
+    let mut last_message = get_messages_count(chat_id, token, 50).await;
     let mut i = 0;
 
     loop {
@@ -68,13 +60,16 @@ fn get_messages_full(chat_id: &str, token: &str) -> String {
             break;
         }
         let parsed_msg = parse_json(&last_message).unwrap();
-        let last_id = parsed_msg[parsed_msg.len()-1]["id"].to_string();
+        let last_id = parsed_msg[parsed_msg.len() - 1]["id"].to_string();
         result.extend(parsed_msg);
-        let last_id = last_id[1..last_id.len()-1].to_string();
+        let last_id = last_id[1..last_id.len() - 1].to_string();
 
-        let url = format!("https://discord.com/api/v9/channels/{}/messages?before={}&limit={}", chat_id, last_id, 50);
+        let url = format!(
+            "https://discord.com/api/v9/channels/{}/messages?before={}&limit={}",
+            chat_id, last_id, 50
+        );
 
-        last_message = discord_api(&url, token);
+        last_message = discord_api(&url, token).await;
         i += 1;
         eprint!("Stage: {}\r", i);
     }
@@ -82,9 +77,12 @@ fn get_messages_full(chat_id: &str, token: &str) -> String {
     to_string(&result).unwrap()
 }
 
-fn get_messages_count(chat_id: &str, token: &str, messages: i32) -> String {
-    let url = format!("https://discord.com/api/v9/channels/{}/messages?limit={}", chat_id, messages);
-    discord_api(&url, token)
+async fn get_messages_count(chat_id: &str, token: &str, messages: i32) -> String {
+    let url = format!(
+        "https://discord.com/api/v9/channels/{}/messages?limit={}",
+        chat_id, messages
+    );
+    discord_api(&url, token).await
 }
 
 pub fn parse_json(messages: &str) -> Result<Vec<Value>, io::Error> {
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,43 @@
-use std::fs;
 use ds_saver::*;
+use std::fs;
 use structopt::StructOpt;
 
 mod console;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let opt = Opt::from_args();
 
     match opt {
         Opt::Chats { token } => {
-            console::print_chats(get_chats(&token));
-        },
-        Opt::Messages { token, chat_id, output, print, full } => {
-            let messages = get_messages(&chat_id, &token, full);
+            console::print_chats(get_chats(&token).await);
+        }
+        Opt::Messages {
+            token,
+            chat_id,
+            output,
+            print,
+            full,
+        } => {
+            let messages = get_messages(&chat_id, &token, full).await;
             if print {
                 console::pretty_print(&messages);
             }
             match output {
                 Some(t) => {
                     fs::write(t, messages).unwrap();
-                },
+                }
                 None => {
                     if !print {
                         fs::write(format!("{}.json", chat_id), messages).unwrap();
-                        println!("Messages saved to {}, check 'messages --help'", format!("{}.json", chat_id))
+                        println!(
+                            "Messages saved to {}, check 'messages --help'",
+                            format!("{}.json", chat_id)
+                        )
                     }
-                },
+                }
             }
-        },
+        }
     }
 }
 
@@ -50,7 +60,11 @@ enum Opt {
         #[structopt(short, long)]
         output: Option<String>,
 
-        #[structopt(short, long, about = "Print messages to screen, if not save to default path")]
+        #[structopt(
+            short,
+            long,
+            about = "Print messages to screen, if not save to default path"
+        )]
         print: bool,
 
         #[structopt(short, long, about = "Save full chat")]
